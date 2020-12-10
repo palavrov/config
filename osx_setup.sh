4b8a5961807c08dd1723b6f08cce66a5185448e0
Atanas Palavrov <palavrov@Atanass-MacBook-Air.local>
2020-12-10 17:11:05 +0100

    brew tap homebrew/cask-drivers

--- osx_setup.sh
+++ osx_setup.sh
@@ -10,6 +10,7 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew tap codigi/homebrew-codigi
+brew tap homebrew/cask-drivers
 brew install bash-completion
 brew install firefox opera google-chrome vivaldi brave-browser dissenter-browser
 brew install skype viber telegram telegram-desktop amazon-chime zoomus slack
@@ -27,13 +28,14 @@ brew install hex-fiend meld xquartz
 brew install paragon-ntfs paragon-extfs
 brew install prolific-pl2303 ftdi-vcp-driver wch-ch34x-usb-serial-driver
 brew install gcc-arm-embedded gdbm
-brew install --cask balenaetcher bluetility packetsender saleae-logic wireshark
+brew install balenaetcher bluetility packetsender saleae-logic wireshark
 # Segger tools
-brew install --cask segger-embedded-studio-for-arm segger-jlink segger-ozone
+brew install segger-embedded-studio-for-arm segger-jlink segger-ozone
 # Nordic tools
-brew install --cask nordic-nrf-command-line-tools nordic-nrf-connect
+brew install nordic-nrf-command-line-tools nordic-nrf-connect
 # Android development
-brew install --cask openjdk android-file-transfer android-sdk android-platform-tools android-studio android-ndk
+brew install openjdk android-file-transfer android-sdk android-platform-tools android-studio android-ndk
+brew install openjdk android-file-transfer android-sdk android-platform-tools android-studio android-ndk
 
 # https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
 git clone --bare git@github.com:palavrov/config.git $HOME/.config.git
